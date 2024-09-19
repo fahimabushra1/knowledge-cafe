646669dc532a8cdb58b7666130d1c7b3dff98a2f
fahima bushra <bushra.arifeen@gmail.com>
2024-09-19 06:17:39 +0600

    1 line code changed

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { 
+    // env:{node: true},
+    ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,13 +1,13 @@
 import profile from "../assets/images/profile.png"
 const Title = () => {
     return (
-        <div>
+        <div className="max-w-7xl mx-auto">
              <div className="flex justify-between items-center p-4">
                 <div>
                         <h1 className="text-3xl text-purple-600 font-bold capitalize">knowledge cafe</h1>
                 </div>
                  <div>
-                      <img className="w-8 max-w-7xl mx-auto" src={profile} alt="profile" />
+                      <img className="w-8" src={profile} alt="profile" />
                  </div>
              </div>
              <hr />
